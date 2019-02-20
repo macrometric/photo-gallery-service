5686b2a67bdfc27f75589eaa3122398602fc13d2
Sarah <sarahrva@gmail.com>
2019-02-20 16:13:04 -0600

    Rename dom element

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Gallery from "./components/App.jsx";
 
-ReactDOM.render(<Gallery />, document.getElementById("gallery"));
+ReactDOM.render(<Gallery />, document.getElementById("photos"));
